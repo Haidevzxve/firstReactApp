35292b52fde5449185f322ed37de939a314df53f
Thanh Hai <thai06314@gmail.com>
2024-05-08 01:11:07 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+      return (
+        <><h1>Hello World</h1><h1> Full Name: Trần Ngô Thanh Hải</h1><h1> Class: SE17B01</h1><h1> School: FPT</h1><h1> Chưa có ngiu</h1></>
+      );
+    }
+  export default App;
+  
